38dcb67043127f8866c251701d9f3f1a76fc2906
fraz <fraz@o2.pl>
2024-12-21 14:16:49 +0100

    [0.7.8][bugfix] product section i18n

--- template-parts/product-section.php
+++ template-parts/product-section.php
@@ -14,13 +14,13 @@ $cta_link   = isset( $args['cta_link'] ) ? $args['cta_link'] : null;
 <aside class="product-section product-section--<?php echo esc_attr( wp_rand() ); ?>"<?php Chocante_Product_Section::output_product_section_atts( $args ); ?>>
 	<header class="product-section__header">
 		<h3 class="product-section__heading">
-			<?php if ( isset( $subheading ) ) : ?>		
+			<?php if ( isset( $subheading ) ) : ?>
 				<span><?php echo esc_html( $subheading ); ?></span>
 			<?php endif; ?>
 			<?php echo esc_html( $heading ); ?>
 		</h3>
 		<?php if ( isset( $cta_link ) ) : ?>
-			<a href="<?php echo esc_attr( $cta_link ); ?>">Zobacz wszystkie</a>
+			<a href="<?php echo esc_attr( $cta_link ); ?>"><?php esc_html_e( 'View all', 'woocommerce' ); ?></a>
 		<?php endif; ?>
 	</header>
 	<div class="product-section__spinner">
