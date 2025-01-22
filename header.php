345c17f59718814b770dace8a63d78f076e00514
fraz <fraz@o2.pl>
2025-01-22 10:33:25 +0100

    [0.13.5][tweak] clean after bricks

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) || exit;
 <html <?php language_attributes(); ?> chocante>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
