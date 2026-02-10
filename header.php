668a94b4459775708299636f30e4632b6220037e
fraz <fraz@o2.pl>
2026-02-10 16:33:07 +0100

    remove obsolete html attr

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> chocante>
+<html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
